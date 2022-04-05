9fec65fb6f3baf75a045169b7ec9853e510cdd31
Emmeken <emmeken@ymail.com>
2022-04-05 03:23:00 +0200

    Add build script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is a [Tweego](https://www.motoslave.net/tweego/) conversion of an [adult Twine game](https://tfgames.site/index.php?module=viewgame&id=2653) made by FloricSpacer, which is in turn based on a CYOA by an anonymous author.
+
+To build:
+- install Git and clone this repo
+- install Tweego and add it to your shell's `PATH`
+- run `./build.sh`
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! which tweego > /dev/null
+then
+    echo "Please install Tweego and add it to your PATH"
+    exit 1
+fi
+
+SUGARCUBE_VERSION=2.36.1
+SUGARCUBE_ARCHIVE="sugarcube-$SUGARCUBE_VERSION-for-twine-2.1-local.zip"
+if ! test -f "storyformats/$SUGARCUBE_ARCHIVE"
+then
+    echo "Downloading SugarCube story format..."
+    curl -L "https://github.com/tmedwards/sugarcube-2/releases/download/v$SUGARCUBE_VERSION/$SUGARCUBE_ARCHIVE" -o "storyformats/$SUGARCUBE_ARCHIVE"
+    echo "Unpacking SugarCube story format..."
+    unzip -d "storyformats/" -o "storyformats/$SUGARCUBE_ARCHIVE"
+fi
