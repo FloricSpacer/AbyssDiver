23ce96e81f819c285ef2af8990b936769c112ede
Ilyza91 <ilsegroot1991@gmail.com>
2023-07-06 09:54:32 +0200

    Bandit identity mix up event surface

--- src/script.js
+++ src/script.js
@@ -127,7 +127,10 @@ Config.navigation.override = function (destPassage) {
 		StoryVar.escapeT = StoryVar.time + 7 + random(0,7);
 		return "Bandit Escape";
 	}
-
+	if (StoryVar.currentLayer == 0 && StoryVar.mc.imageIcon == "Icons/BanditIcon_released.jpg" &&  StoryVar.mc.inhuman<6 && StoryVar.mc.appAge > 12 && !StoryVar.arrested) {
+		return "Bandit Arrested";
+	}
+	currentLayer
 	if (isFinite(StoryVar.mc.appAge) && StoryVar.mc.appAge < 3 && StoryVar.mc.age > 17) {
 		return "AgeEnd";
 	}
