db61d96c0743f50fb46df8f0b741579918362f4b
Annonymus <marcotereh97@gmail.com>
2023-11-08 11:18:01 +0100

    use toggles for passive relics

--- src/htmlComponents.js
+++ src/htmlComponents.js
@@ -1,4 +1,4 @@
-/* exported toggle */
+/* exported toggle, select */
 /**
  * Creates an on/off UI element.
  * The element has the following additional functions:
@@ -58,3 +58,59 @@ function toggle(cb, init) {
 	return container;
 }
 window.toggle = toggle;
+
+/**
+ * Creates a multiple choice UI element.
+ * The element has the following additional functions:
+ * - select(value) selects the given value. This will call the callback, so beware of infinite recursion.
+ * - state() returns the current state of the element.
+ * The provided states can be an array of strings, in which case the same string will be used as value or an
+ * object containing the mappings from string to value, in which case the object's keys will be displayed
+ * in the UI and the corresponding values sent to the callback upon selection.
+ * @param {function} cb The callback to call when the state is changed.
+ * @param {[string]|Object.<String, any>} vals The possible values of the form element.
+ * @param {any} init The initial value of the toggle.
+ * @returns {HTMLElement} An HTMLElement containing the toggle.
+ */
+function select(cb, vals, init) {
+	let container = document.createElement('div');
+	container.classList.add('toggle');
+
+	let map = vals;
+	if (Array.isArray(vals)) {
+		map = {}
+		for (let val of vals) {
+			map[val] = val;
+		}
+	}
+
+	let elems = []
+	let selected = init;
+
+	function select(val) {
+		selected = val;
+		for (let elem of elems) {
+			elem[0].classList.remove('selected');
+			if (elem[1] === val) {
+				elem[0].classList.add('selected');
+			}
+		}
+		cb(val);
+	}
+	container.select = select;
+	function state() {return selected}
+	container.state = state;
+
+	for (let val in map) {
+		let optionElem = document.createElement('div');
+		optionElem.textContent = val;
+		optionElem.classList.add('toggle-item');
+		if (init === map[val]) optionElem.classList.add('selected');
+		container.append(optionElem);
+		elems.push([optionElem, map[val]]);
+		optionElem.addEventListener('click', () => select(map[val]));
+	}
+
+	return container;
+}
+window.select = select;
