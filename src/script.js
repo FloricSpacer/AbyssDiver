58b2f2285629d0334c3d3c84bd72cd55776578dd
FloricSpacer <104052386+FloricSpacer@users.noreply.github.com>
2024-08-30 22:50:23 -0400

    Edited outdated settings labels

--- src/script.js
+++ src/script.js
@@ -412,17 +412,17 @@ Setting.addToggle("EncounterFiat", {
 Setting.addHeader("Content Settings");
 
 Setting.addToggle("MaleSceneToggleFilter", {
-    label : "Enable random sex scenes involving male characters",
+    label : "Enable sex scenes involving male characters",
     default  : true,
 });
 
 Setting.addToggle("FemaleSceneToggleFilter", {
-    label : "Enable random sex scenes involving female characters",
+    label : "Enable sex scenes involving female characters",
     default  : true,
 });
 
 Setting.addToggle("OtherSceneToggleFilter", {
-    label : "Enable random sex scenes involving futa characters or characters without genitals",
+    label : "Enable sex scenes involving futa characters or characters without genitals",
     default  : true,
 });
 
