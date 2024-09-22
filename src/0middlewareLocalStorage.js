0e1b2080a14176e35290bb1f59d1ca070abb76ad
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-09-22 12:27:43 +0930

    trim-saves-in-testing

--- src/0middlewareLocalStorage.js
+++ src/0middlewareLocalStorage.js
@@ -6,7 +6,7 @@ const originalLocalStorage = window.localStorage;
 
 const LocalStorageMiddleware = {
 	log(action, key, value) {
-		console.log(`Action: ${action}, Key: ${key}, Value: ${value}`);
+		//console.log(`Action: ${action}, Key: ${key}, Value: ${value}`);
 	},
 	validate(key, value) {
 		if (value === null || value === undefined) {
@@ -17,14 +17,14 @@ const LocalStorageMiddleware = {
 	},
 	setItem(key, value) {
 		if (this.validate(key, value)) {
-			originalLocalStorage.setItem(key, JSON.stringify(value));
+			originalLocalStorage.setItem(key, value);
 			this.log('set', key, value);
 		}
 	},
 	getItem(key) {
 		const value = originalLocalStorage.getItem(key);
 		this.log('get', key, value);
-		return value ? JSON.parse(value) : null;
+		return value;
 	},
 	removeItem(key) {
 		this.log('remove', key, originalLocalStorage.getItem(key));
@@ -50,12 +50,56 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 const SaveFileTrimmer = {
-	trim(data) {
+	trim_state(state) {
+		for (const state_delta of state.delta) {
+			// trim static relic variables
+			delete state_delta.relics;
+			// trim static companion variables
+			delete state_delta.companions;
+			// trim multi-static variables
+			const array_keys = Object.keys(state_delta.variables);
+			for (const key of array_keys) {
+				// curse#, relic#, item#
+				if (key.match(/curse\d+/) != null || key.match(/relic\d+/) != null || key.match(/item\d+/) != null) {
+					delete state_delta.variables[key];
+				}
+				// companionNAME
+				/*if (key.includes("companion") && key != "companions") {
+					delete state_delta.variables[key];
+				}*/
+			}
+		}
+	},
+
+	untrim_state(state) {
 
 	},
 
-	untrim(data) {
+	iter_save_states(data, callback) {
+		// autosave trim
+		if (Object.hasOwn(data, 'autosave')) {
+			callback(data.autosave.state);
+		}
+		// save slot trims
+		if (Object.hasOwn(data, 'slots')) {
+			for (const slot_data of data.slots) {
+				if (slot_data != null) {
+					callback(slot_data.state);
+				}
+			}
+		}
+	},
 
+	trim(data) {
+		console.log("trim save file");
+		console.log(data);
+		SaveFileTrimmer.iter_save_states(data, SaveFileTrimmer.trim_state);
+	},
+
+	untrim(data) {
+		console.log("untrim save file");
+		console.log(data);
+		SaveFileTrimmer.iter_save_states(data, SaveFileTrimmer.untrim_state);
 	}
 }
 
@@ -71,19 +115,33 @@ window.updateSugarCubeStorageMiddleware = () => {
 	}
 	if (window.SugarCube && window.SugarCube.storage) {
 		window.hasDefinedMiddleware = true;
+		console.log('SugarCube storage middleware setup.');
+		
 		// const original_set = window.SugarCube.storage.set;
 		window.SugarCube.storage.set = function(key, value) {
-			console.log("storage - set - ", key, value);
-			return LocalStorageMiddleware.setItem(key, value);
-			// return original_set.call(this, key, value);
+			console.log("storage - set - ", key);
+			try {
+				SaveFileTrimmer.trim(value);
+				const serial_value = window.SugarCube.storage.constructor._serialize(value);
+				LocalStorageMiddleware.setItem(window.SugarCube.storage._prefix + key, serial_value);
+				return true;
+			} catch (ex) {
+				console.error(ex);
+				return false;
+			}
 		}
+
 		// const original_get = window.SugarCube.storage.get;
 		window.SugarCube.storage.get = function(key) {
 			console.log("storage - get - ", key);
-			return LocalStorageMiddleware.getItem(key);
-			//return original_get.call(this, key);
+			const saveRaw = LocalStorageMiddleware.getItem(window.SugarCube.storage._prefix + key);
+			const saveObject = saveRaw == null ? null : window.SugarCube.storage.constructor._deserialize(saveRaw);
+			if (saveObject != null) {
+				SaveFileTrimmer.untrim(saveObject);
+			}
+			return saveObject;
 		}
-		console.log('SugarCube storage middleware updated.');
+
 	} else {
 		console.warn('SugarCube is not loaded.');
 	}
--- temp/trim_test.py
+++ temp/trim_test.py
@@ -1,12 +1,55 @@
 
-
+# TODO:
+# trim static variables
+# trim curses
+# trim items
+# trim vault
+# trim companions
 
 import json
 import re
 
-with open('temp/raw_save.json', 'r') as file:
+with open('temp/test_actual_save.json', 'r') as file:
 	data  = json.loads(file.read())
 
+# parse item arrays
+def parse_item_array(vault_items) -> None:
+	if isinstance(vault_items[0], int):
+		vault_items = vault_items[1]
+	
+	if isinstance(vault_items[0], int):
+		vault_items = vault_items[1]
+
+	for value in vault_items:
+		if isinstance(value, dict) and value['count'] == 0:
+			vault_items.remove(value)
+
+# def trim_companions(hired : list[str], companions : list) -> None:
+# 	pass
+
+# parse each delta state
+def parse_state_delta(delta):
+	for item in delta:
+
+		item['variables'].pop("relics", None)
+		item['variables'].pop("companions", None)
+
+		# if item['variables'].get("vaultItems"):
+		# 	parse_item_array(item['variables']["vaultItems"])
+
+		# if item['variables'].get("items"):
+		# 	parse_item_array(item['variables']["items"])
+
+		keyz = list(item['variables'].keys())
+		for key in keyz:
+			# any curse#, relic#, item# are removed
+			if re.match("curse\d+", key) or re.match("relic\d+", key) or re.match("item\d+", key):
+				item['variables'].pop(key, None)
+
+			if "companion" in key and key != "companions":
+				item['variables'].pop(key, None)
+
+# iterate each save slot
 for slot in data['slots']:
 	if slot is None:
 		continue
@@ -14,13 +57,8 @@ for slot in data['slots']:
 		continue
 	if slot['state'].get('delta') is None:
 		continue
-	for item in slot['state']['delta']:
-		keyz = list(item['variables'].keys())
-		for key in keyz:
-			if re.match("curse\d*", key) or re.match("relic\d*", key) or re.match("item\d*", key) or "companion" in key:
-				item['variables'].pop(key, None)
-			if key == "relics" or key == "vaultItems" or key == "companions":
-				item['variables'].pop(key, None)
+	parse_state_delta(slot['state']['delta'])
 
-with open('temp/trimmed.json', 'w') as file:
+# write to file
+with open('temp/test_actual_save_trimmed.json', 'w') as file:
 	file.write(json.dumps(data,indent=4))
