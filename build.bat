2fb1138e8220c955967e7fc7f53de60c2155ad16
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-11-17 04:43:29 +0930

    Revert "attempt-string-terminator-fix"
    This reverts commit c121c6a3b653500c93d61c84d6eba42a499b4e66.

--- build.bat
+++ build.bat
@@ -7,51 +7,51 @@ set "TWEEGO=tweego"
 where "!TWEEGO!" >nul 2>nul
 
 if "!errorlevel!" == "1" (
-	set "TWEEGO_VERSION=2.1.1"
-	echo TWEEGO_VERSION: !TWEEGO_VERSION!
-	set "TWEEGO_OS=windows"
-	echo TWEEGO_OS: !TWEEGO_OS!
-	set "TWEEGO_ARCH=x64"
+    set "TWEEGO_VERSION=2.1.1"
+    echo TWEEGO_VERSION: !TWEEGO_VERSION!
+    set "TWEEGO_OS=windows"
+    echo TWEEGO_OS: !TWEEGO_OS!
+    set "TWEEGO_ARCH=x64"
 
-	echo PROCESSOR_ARCHITECTURE: !PROCESSOR_ARCHITECTURE!
-	if !PROCESSOR_ARCHITECTURE!==x86 (
-		set "TWEEGO_ARCH=x86"
-	) else if !PROCESSOR_ARCHITECTURE!==AMD64 (
-		set "TWEEGO_ARCH=x64"
-	) else (
-		echo No pre-built Tweego is available for CPU family !PROCESSOR_ARCHITECTURE!.
-		echo Please build Tweego from source and either add it to your PATH or put its location in the TWEEGO environment variable.
-		exit /b 1
-	)
+    echo PROCESSOR_ARCHITECTURE: !PROCESSOR_ARCHITECTURE!
+    if !PROCESSOR_ARCHITECTURE!==x86 (
+        set "TWEEGO_ARCH=x86"
+    ) else if !PROCESSOR_ARCHITECTURE!==AMD64 (
+        set "TWEEGO_ARCH=x64"
+    ) else (
+        echo No pre-built Tweego is available for CPU family !PROCESSOR_ARCHITECTURE!.
+        echo Please build Tweego from source and either add it to your PATH or put its location in the TWEEGO environment variable.
+        exit /b 1
+    )
 
-	echo TWEEGO_ARCHITECTURE: !TWEEGO_ARCH!
+    echo TWEEGO_ARCHITECTURE: !TWEEGO_ARCH!
 
-	set "TWEEGO_ARCHIVE=tweego-!TWEEGO_VERSION!-!TWEEGO_OS!-!TWEEGO_ARCH!.zip"
-	echo !TWEEGO_ARCHIVE!
+    set "TWEEGO_ARCHIVE=tweego-!TWEEGO_VERSION!-!TWEEGO_OS!-!TWEEGO_ARCH!.zip"
+    echo !TWEEGO_ARCHIVE!
 
-	if not exist "!WORKAREA!\tools\!TWEEGO_ARCHIVE!" (
-		echo Downloading Tweego...
-		curl -L "https://github.com/tmedwards/tweego/releases/download/v!TWEEGO_VERSION!/!TWEEGO_ARCHIVE!" -o "!WORKAREA!\tools\!TWEEGO_ARCHIVE!"
+    if not exist "!WORKAREA!\tools\!TWEEGO_ARCHIVE!" (
+        echo Downloading Tweego...
+        curl -L "https://github.com/tmedwards/tweego/releases/download/v!TWEEGO_VERSION!/!TWEEGO_ARCHIVE!" -o "!WORKAREA!\tools\!TWEEGO_ARCHIVE!"
 
-		echo Unpacking Tweego...
-		powershell -command "Expand-Archive -Path '!WORKAREA!\tools\!TWEEGO_ARCHIVE!' -DestinationPath '!WORKAREA!\tools\' -Force"
-	)
+        echo Unpacking Tweego...
+        powershell -command "Expand-Archive -Path '!WORKAREA!\tools\!TWEEGO_ARCHIVE!' -DestinationPath '!WORKAREA!\tools\' -Force"
+    )
 
-	set "TWEEGO=!WORKAREA!tools/tweego.exe"
-	echo Using downloaded Tweego: !TWEEGO!
+    set "TWEEGO=!WORKAREA!tools/tweego.exe"
+    echo Using downloaded Tweego: !TWEEGO!
 ) else (
-	set "TWEEGO=tweego"
-	echo Using systemwide Tweego: !TWEEGO!
+    set "TWEEGO=tweego"
+    echo Using systemwide Tweego: !TWEEGO!
 )
 
 set "SUGARCUBE_VERSION=2.37.0"
 set "SUGARCUBE_ARCHIVE=sugarcube-!SUGARCUBE_VERSION!-for-twine-2.1-local.zip"
 
 if not exist "!WORKAREA!storyformats\!SUGARCUBE_ARCHIVE!" (
-	echo Downloading SugarCube story format...
-	powershell -Command "Invoke-WebRequest -Uri \"https://github.com/tmedwards/sugarcube-2/releases/download/v!SUGARCUBE_VERSION!/!SUGARCUBE_ARCHIVE!\" -OutFile \"!WORKAREA!storyformats\!SUGARCUBE_ARCHIVE!\""
-	echo Unpacking SugarCube story format...
-	powershell -Command "Expand-Archive -Path \"!WORKAREA!storyformats\!SUGARCUBE_ARCHIVE!\" -DestinationPath \"!WORKAREA!storyformats\\\" -Force"
+    echo Downloading SugarCube story format...
+    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/tmedwards/sugarcube-2/releases/download/v!SUGARCUBE_VERSION!/!SUGARCUBE_ARCHIVE!' -OutFile '!WORKAREA!storyformats\!SUGARCUBE_ARCHIVE!'"
+    echo Unpacking SugarCube story format...
+    powershell -Command "Expand-Archive -Path '!WORKAREA!storyformats\!SUGARCUBE_ARCHIVE!' -DestinationPath '!WORKAREA!storyformats\' -Force"
 )
 
 set "OUTPUT=AbyssDiver.html"
