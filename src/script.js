ddee6ccc8d398711cc421c75323541be844e9377
FloricSpacer <104052386+FloricSpacer@users.noreply.github.com>
2024-12-02 21:56:48 -0500

    Removed excess setting

--- src/script.js
+++ src/script.js
@@ -452,12 +452,6 @@ Setting.addToggle("AIPortraitsMode", {
     label: "Enable the use of AI to generate your own portrait.",
     default: false,
 });
-
-Setting.addToggle("SidebarPortrait", {
-    label: "Enable the use of the sidebar portrait for generated images.",
-    default: true,
-});
-
 /*
 Setting.addToggle("SidebarPortrait", {
     label: "Enable the use of the sidebar portrait for generated images.",
@@ -1949,7 +1943,7 @@ Macro.add('sidebar-widget', {
             $('body').prepend(toggleButton);
         }
 
-        if (settings.SidebarPortrait && !settings.OverridePortrait && setup.firstPortraitGen) {
+        if (!settings.OverridePortrait && setup.firstPortraitGen) {
             setup.displaySavedImage();
         }
 
